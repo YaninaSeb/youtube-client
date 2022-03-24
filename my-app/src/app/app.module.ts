88ee629ee1fbab01e6c079a5fbdb004d2f3e2a46
Yanina Sebrukovich <sebrukovich_jana@mail.ru>
2022-03-24 23:22:00 +0300

    feat: add components for home page

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
+import { SearchInputComponent } from './header/search-input/search-input.component';
+import { LoginComponent } from './header/login/login.component';
+import { SettingsComponent } from './header/settings/settings.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { FiltersComponent } from './filters/filters.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SearchComponent,
+    SearchInputComponent,
+    LoginComponent,
+    SettingsComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    FiltersComponent
   ],
   imports: [
     BrowserModule,
